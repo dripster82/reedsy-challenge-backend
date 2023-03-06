5605b9c1a94def5d57f3e295883f1d986a7d3ee1
Paul Ketelle <paul.ketelle@vodafone.com>
2023-03-06 00:15:52 +0000

    Testing Successful CI tests

--- spec/app/controllers/ping_controller_spec.rb
+++ spec/app/controllers/ping_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::PingController', type: :request do
   describe '#index' do
-    it 'returns pong' do
+    it 'returns pong as the body' do
       get '/api/v1/ping'
 
       expect(response.body).to eq 'pong'
